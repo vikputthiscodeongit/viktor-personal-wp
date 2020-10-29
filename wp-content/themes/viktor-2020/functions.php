64b0dabbaec599a3c5b276dc4fa6bc6b6ea8f923
vikputthiscodeongit <vikputthiscodeongit@users.noreply.github.com>
2020-10-29 23:08:02 +0100

    Change WordPress update policies

--- wp-content/themes/viktor-2020/functions.php
+++ wp-content/themes/viktor-2020/functions.php
@@ -7,8 +7,7 @@
 
     //
     // Updates
-    add_filter("allow_major_auto_core_updates", "__return_false");
-    add_filter("auto_update_plugin", "__return_false");
+    add_filter("auto_update_theme", "__return_true");
 
 
     //
